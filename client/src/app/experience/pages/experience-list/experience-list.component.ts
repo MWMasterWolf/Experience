7aa0c33171fd40f61435c2c900d82d438ed0f4ab
Nelson Almonte <almontetejedanelson@gmail.com>
2022-01-26 12:54:01 -0400

    Working project

--- client/src/app/experience/pages/experience-list/experience-list.component.ts
+++ client/src/app/experience/pages/experience-list/experience-list.component.ts
@@ -5,7 +5,7 @@ import { ExperienceService } from '../../services/experience.service';
 // Interfaces
 import { Experience, ApiResponse } from '../../interfaces/experience.interface';
 // Third-party
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-experience-list',
@@ -30,26 +30,35 @@ export class ExperienceListComponent implements OnInit {
   }
 
   deleteExperience(id: string): void {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.experienceService.deleteExperience(id).subscribe((response: Experience) => {
-          this.experienceService.refresh();
-          this.router.navigate(['/experiences']);
-          Swal.fire(
-            'Deleted!',
-            'Experience deleted.',
-            'success'
-          )
-        });
-      }
-    })
+    this.experienceService.deleteExperience(id).subscribe((response: Experience) => {
+      this.experienceService.refresh();
+      this.router.navigate(['/experiences']);
+      // Swal.fire(
+      //   'Deleted!',
+      //   'Experience deleted.',
+      //   'success'
+      // )
+    });
+    // Swal.fire({
+    //   title: 'Are you sure?',
+    //   text: "You won't be able to revert this!",
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Yes, delete it!'
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     this.experienceService.deleteExperience(id).subscribe((response: Experience) => {
+    //       this.experienceService.refresh();
+    //       this.router.navigate(['/experiences']);
+    //       Swal.fire(
+    //         'Deleted!',
+    //         'Experience deleted.',
+    //         'success'
+    //       )
+    //     });
+    //   }
+    // })
   }
 }
